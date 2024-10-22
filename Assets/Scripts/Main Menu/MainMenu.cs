0e95716c0be26ef0cc0feb728b0304deeaa6af93
Santiago <santiagomorales333@yahoo.com>
2024-10-22 00:09:31 -0700

    created UI for authentication. Need to implement web requests

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -5,18 +5,66 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-	public void LoadLevel()
-	{
-		SceneManager.LoadScene("Game");
-	}
-	public void LoadMainMenu()
-	{
-		Time.timeScale = 1;
-		SceneManager.LoadScene("MainMenu");
-	}
-	public void Quit()
-	{
-		Application.Quit();
-	}
-  //load the main menu
+    private GameObject loginPanel, signupPanel, loginSignupPanel;
+
+    private void Start()
+    {
+        loginPanel = GameObject.Find("login_panel");
+        signupPanel = GameObject.Find("signup_panel");
+        loginSignupPanel = GameObject.Find("login_signup_panel");
+
+        if (loginPanel != null && signupPanel != null && loginSignupPanel != null)
+        {
+            loginPanel.SetActive(false);
+            signupPanel.SetActive(false);
+
+        }
+    }
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    /*
+     Navigates from the main menu to either the Login or Signup Panel. 
+     Also navigates back to main menu. Normally, I would have condensed
+     these methods into one but OnClick does not support mult. params.
+     */
+    public void NavigateToPanel(string panelName)
+    {
+
+        loginSignupPanel.SetActive(false);
+
+        if (panelName == loginPanel.name)
+            loginPanel.SetActive(true);
+        else if (panelName == signupPanel.name)
+            signupPanel.SetActive(true);
+
+
+
+
+    }
+    public void NavigateFromPanel(string panelName)
+    {
+
+        loginSignupPanel.SetActive(true);
+
+        if (panelName == loginPanel.name)
+            loginPanel.SetActive(false);
+        else if (panelName == signupPanel.name)
+            signupPanel.SetActive(false);
+
+
+
+    }
+
+
 }
